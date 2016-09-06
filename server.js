a276d81f9a2258a14aeae88d65a12595189e7e3f
Diogo-Queiroz <diogoqueirooz1212@gmail.com>
2016-09-06 00:23:01 -0300

    ready to run

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ app.set('view engine', 'jade');
 app.get('*', function (req, res) {
     res.render('index');
 })
+
+var port = 3030;
+app.listen(port);
+
+console.log('Listening on port ' + port + "...");
