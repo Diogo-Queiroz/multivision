100278f2a19a6b28cba484355a26e1026a99118a
Diogo-Queiroz <diogoqueirooz1212@gmail.com>
2016-09-19 17:38:46 -0300

    Continue to implement login display

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ db.once('open', function callback() {
 	console.log('multivision db opened!');
 });
 
-app.get('/partials/:partialPath', function(req, res){
-	res.render('partials/' + req.params.partialPath);
+app.get('/partials/*', function(req, res){
+	res.render('partials/' + req.params[0]);
 });
 
 app.get('*', function (req, res) {
