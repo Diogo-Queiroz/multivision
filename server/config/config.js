bcb84f5efea73e04fbd69fddda6e316e462dd7f0
Diogo-Queiroz <diogoqueirooz1212@gmail.com>
2016-09-19 23:55:04 -0300

    refactoring server.js

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var rootPath = path.normalize(__dirname + '../../');
+
+module.exports = {
+    developemtn: {
+        db:'mongodb://localhost/multivision',
+        rootPath: rootPath,
+        port: process.env.PORT || 3030
+    },
+    production: {
+        db:'mongodb://diogo-admin:multivision@ds033116.mlab.com:33116/plural-multivision',
+        rootPath: rootPath,
+        port: process.env.PORT || 80
+    }
+};
