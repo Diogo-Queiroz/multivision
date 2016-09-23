1ac244e737b1be95b3d6587cb1417477a6b3f21a
Diogo-Queiroz <diogoqueirooz1212@gmail.com>
2016-09-23 16:22:42 -0300

    Complete the Improving Client Login Code, has forget one step

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -7,15 +7,6 @@ angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, m
             } else {
                 mvNotifier.notify('Username/Password combination incorrect');
             }
-        })
-
-        $http.post('/login', {username:username, password:password}).then(function (response) {
-            if(response.data.success){
-                mvIdentity.currentUser = response.data.user;
-                mvNotifier.notify('You have successfully signed in!');
-            } else {
-                mvNotifier.notify('Username/Password combination incorrect');
-            }
-        })
+        });
     }
 });
