f9dd1b54d25d788fcbb04770eca2886183a9469b
Diogo-Queiroz <diogoqueirooz1212@gmail.com>
2016-10-25 23:49:57 -0200

    Implementação do SignUp no Client-Side e Server-Side, falta refatorar o código, porém ainda contem algum erro, aqui não está encontrando o route /api/users

--- public/app/app.js
+++ public/app/app.js
@@ -5,31 +5,35 @@ angular.module('app').config(function($routeProvider, $locationProvider){
 	Variável responsável por guardar o valor se um usuário tem permissão de
 
 	 */
-    var routeRoleCheck = {
-        admin: {
-            auth: function (mvAuth) {
-                return mvAuth.authorizeCurrentUserForRoute('admin');
-            }
-        }
-    };
+  var routeRoleCheck = {
+      admin: {
+          auth: function (mvAuth) {
+              return mvAuth.authorizeCurrentUserForRoute('admin');
+          }
+      }
+  };
 
-    $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 	$routeProvider
 		.when('/', {
 			templateUrl: '/partials/main/main',
 			controller: 'myMainCtrl'
 		})
-        .when('/admin/users', {
-            templateUrl: '/partials/admin/user-list',
-            controller: 'mvUserListCtrl',
-            resolve: routeRoleCheck.admin
-        })
+		.when('/admin/users', {
+      templateUrl: '/partials/admin/user-list',
+      controller: 'mvUserListCtrl',
+      resolve: routeRoleCheck.admin
+    })
+		.when('/signup', {
+			templateUrl: '/partials/account/signup',
+			controller: 'mvSignupCtrl'
+		})
 });
 
 angular.module('app').run(function ($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
-        if(rejection === 'not authorized'){
-            $location.path('/');
-        }
-    })
-})
+  $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+    if(rejection === 'not authorized'){
+      $location.path('/');
+    }
+  })
+});
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,74 +1,63 @@
 var mongoose = require('mongoose'),
-    crypto = require('crypto');
+    encrypt = require('../utilities/encryption.js');
 
 module.exports = function (config) {
-    mongoose.connect(config.db);
+  mongoose.connect(config.db);
 
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error...'));
-    db.once('open', function callback() {
-        console.log('multivision db opened!');
-    });
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error...'));
+  db.once('open', function callback() {
+    console.log('multivision db opened!');
+  });
 
-    var userSchema = mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        username: String,
-        salt: String,
-        hashed_pwd: String,
-        roles: [String]
-    });
-    userSchema.methods = {
-        authenticate: function (passwordToMatch) {
-            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-        }
-    };
+  var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: String,
+    salt: String,
+    hashed_pwd: String,
+    roles: [String]
+  });
+  userSchema.methods = {
+    authenticate: function (passwordToMatch) {
+      return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  };
 
-    var User = mongoose.model('User', userSchema);
-    User.find({}).exec(function (err, collection) {
-        if(collection.length === 0){
-            var salt, hash;
-            salt = createSalt();
-            hash = hashPwd(salt, 'DQX1212');
-            User.create({
-                firstName: 'Diogo',
-                lastName: 'Queiroz',
-                username: 'DQX1212',
-                salt: salt,
-                hashed_pwd: hash,
-                roles: ["admin"]
-            });
-            salt = createSalt();
-            hash = hashPwd(salt, 'andressaQ');
-            User.create({
-                firstName: 'Andressa',
-                lastName: 'Queiroz',
-                username: 'andressaQ',
-                salt: salt,
-                hashed_pwd: hash,
-                roles: []
-            });
-            salt = createSalt();
-            hash = hashPwd(salt, 'joe');
-            User.create({
-                firstName: 'Joe',
-                lastName: 'Eames',
-                username: 'joe',
-                salt: salt,
-                hashed_pwd: hash
-            });
-        }
-    })
+  var User = mongoose.model('User', userSchema);
+  User.find({}).exec(function (err, collection) {
+    if(collection.length === 0){
+      var salt, hash;
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'DQX1212');
+      User.create({
+        firstName: 'Diogo',
+        lastName: 'Queiroz',
+        username: 'DQX1212',
+        salt: salt,
+        hashed_pwd: hash,
+        roles: ["admin"]
+      });
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'andressaQ');
+      User.create({
+        firstName: 'Andressa',
+        lastName: 'Queiroz',
+        username: 'andressaQ',
+        salt: salt,
+        hashed_pwd: hash,
+        roles: []
+      });
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'joe');
+      User.create({
+        firstName: 'Joe',
+        lastName: 'Eames',
+        username: 'joe',
+        salt: salt,
+        hashed_pwd: hash
+      });
+    }
+  })
 };
 
-function createSalt(){
-    return crypto.randomBytes(128).toString('base64');
-}
-
-function hashPwd(salt, pwd){
-    var hmac = crypto.createHmac('sha1', salt);
-    hmac.setEncoding('hex');
-    hmac.write(pwd);
-    hmac.end();
-    return hmac.read();
-}
